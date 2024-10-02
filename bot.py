ae128187448ad119cd6379d37bc57a3fb7d3348e
ibrahem <ibrahem.al-radaei@outlook.com>
2024-10-02 04:02:05 +0300

    .

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from . import bot as MA_MAIN # MA_MAIN.event(tab, **kwargs)
 
 from . import main
 from .media_bots import films_bot  # test_films
-
+# from .jobs_bots.test4_bots.langs_w import Lang_work
 
 def event(tab, return_no_labs=False, **kwargs):
     return main.event(tab, return_no_labs=return_no_labs, **kwargs)
--- jobs_bots/test4_bots/langs_w.py
+++ jobs_bots/test4_bots/langs_w.py
@@ -8,6 +8,11 @@ from .langs_w import Lang_work
 # ---
 from ...ma_lists_bots import (
     All_Nat,
+    Films_keys_both_new,
+    Films_key_333,
+    film_Keys_For_female,
+    Films_key_CAO,
+    Films_key_For_nat,
     Jobs_key_mens,
     languages_key,
     lang_key_m,
@@ -49,7 +54,7 @@ def Lang_work(con_3):
     # ---
     for wriff, Wriff_lab in tta.items():
         if con_3.startswith(wriff) and lang_lab == "":
-            con_43 = con_3[len(wriff):].strip()
+            con_43 = con_3[len(wriff) :].strip()
             lang_lac = languages_key.get(f"{con_43} language", "")
             print_put(con_43)
             if lang_lac:
@@ -73,23 +78,70 @@ def Lang_work(con_3):
             output_test4(f"<<lightblue>> con_3.startswith(lang:{lang2})")
             output_test4(f"<<lightblue>> con_3.startswith(lang:{lang2})")
             # ---
-            if All_Nat.get(lang, False):
-                nat_labe = All_Nat[lang]["mens"]
-                output_test4(f'<<lightred>> skip lang:"{lang}" in All_Nat,l_lab:"{l_lab}",nat_labe:"{nat_labe}" ')
-            else:
-                con_8 = con_3[len(lang2):]
-                con_78_lab = Jobs_key_mens.get(con_8, "")
-                if con_78_lab:
-                    lang_lab = f"{con_78_lab} ب{languages_key[lang]}"  # languages_key[lang].format(con_78_lab)
-                    output_test4(f'<<lightblue>> con_3.startswith_priff2("{lang2}"), lang_lab:"{lang_lab}"')
-                # ---
-                else:
-                    con_78_lab = lang_key_m.get(con_8, "")
-                    # ---
-                    if con_78_lab:
-                        output_test4(f'<<lightblue>> con_3.startswith_lang("{lang}"), con_78_lab:"{con_78_lab}"')
-                        lang_lab = lang_key_m[con_8].format(languages_key[lang])
+            lang_lab = lab_from_lang_keys(con_3, lang, l_lab, lang2)
     # ---
     Lang_work_cash[cash_key] = lang_lab
     # ---
     return lang_lab
+
+
+def lab_from_lang_keys(con_3, lang, l_lab, lang2):
+    # ---
+    if All_Nat.get(lang, False):
+        nat_labe = All_Nat[lang]["mens"]
+        output_test4(f'<<lightred>> skip lang:"{lang}" in All_Nat,l_lab:"{l_lab}",nat_labe:"{nat_labe}" ')
+        return ""
+    # ---
+    language_lab = languages_key[lang]
+    # ---
+    con_8 = con_3[len(lang2) :]
+    con_78_lab = Jobs_key_mens.get(con_8, "")
+    # ---
+    it_lab = ""
+    # ---
+    if con_78_lab:
+        it_lab = f"{con_78_lab} ب{language_lab}"  # language_lab.format(con_78_lab)
+        output_test4(f'<<lightblue>> Jobs_key_mens({con_8}): con_3.startswith_priff2("{lang2}"), it_lab:"{it_lab}"')
+    # ---
+    if not it_lab:
+        con_78_lab = lang_key_m.get(con_8, "")
+        # ---
+        if con_78_lab:
+            output_test4(f'<<lightblue>> lang_key_m({con_8}), con_78_lab:"{con_78_lab}"')
+            it_lab = lang_key_m[con_8].format(language_lab)
+    # ---
+    if not it_lab:
+        output_test4(f"no match for :({con_8}), {language_lab=}")
+        if Films_key_For_nat.get(con_8):
+            it_lab = Films_key_For_nat[con_8].format(f"ب{language_lab}")
+            output_test4(f'<<lightblue>> lab_from_lang_keys Films_key_For_nat. it_lab:"{it_lab}"')
+    # ---
+    if not it_lab:
+        if con_8.endswith(" films"):
+            # ---
+            con_9 = con_8[: -len("films")].strip().lower()
+            con_9_lab = Films_keys_both_new.get(con_9, {}).get("female", "")
+            # ---
+            # print(f"({con_8}):({con_9}):({con_9_lab})\n" * 10)
+            # ---
+            if con_9_lab:
+                it_lab = f"أفلام {con_9_lab} ب{language_lab}"
+                output_test4(f'<<lightblue>> lab_from_lang_keys Films_key_333. it_lab:"{it_lab}"')
+    # ---
+    dict_tabs = {
+        "film_Keys_For_female": film_Keys_For_female,
+        # "Films_keys_both_new": Films_keys_both_new,
+        "Films_key_333": Films_key_333,
+        "Films_key_CAO": Films_key_CAO,
+    }
+    # ---
+    if not it_lab:
+        for key, dict_tab in dict_tabs.items():
+            con_78_lab = dict_tab.get(con_8)
+            if con_78_lab:
+                # ---
+                it_lab = f"{con_78_lab} ب{language_lab}"
+                output_test4(f'<<lightblue>> lab_from_lang_keys {key}. it_lab:"{it_lab}"')
+                break
+    # ---
+    return it_lab
