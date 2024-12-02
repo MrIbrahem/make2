fdab03d0e494add6dead551ce3d4434f0225742d
ibrahem <ibrahem.al-radaei@outlook.com>
2024-12-02 03:37:56 +0300

    Update fax2.py

--- ma_bots/fax2.py
+++ ma_bots/fax2.py
@@ -11,28 +11,86 @@ from .fax2_bots.squad_title_bot import get_squad_title
 Find_stubs = {1: True if "-stubs" in sys.argv else False}
 
 # tab[Category:21st-century members of the Louisiana State Legislature] = "تصنيف:أعضاء القرن 21 هيئة ولاية لويزيانا التشريعية"
+# Category:Association football matches navigational boxes by teams:Egypt
+"""
+
+elif category3.endswith(" sports navigational boxes"):
+    list_of_cat = "صناديق تصفح الرياضة في {}"
+    category3 = category3.replace(" sports navigational boxes", "", 1)
+
+elif category3.endswith(" navigational boxes"):
+    list_of_cat = "صناديق تصفح {}"
+    category3 = category3.replace(" navigational boxes", "", 1)
+
+"""
+to_get_endswith = {
+    "sports navigational boxes": {
+        "lab": "صناديق تصفح الرياضة في {}",
+        "Find_wd": False,
+        "example": "Category:Yemen sports navigational boxes",
+    },
+    "navigational boxes": {
+        "lab": "صناديق تصفح {}",
+        "Find_wd": False,
+        "example": "",
+    },
+    "alumni": {
+        "lab": "خريجو {}",
+        "Find_wd": True,
+        "example": "",
+    },
+    "board members": {
+        "lab": "أعضاء مجلس {}",
+        "Find_wd": True,
+        "example": "",
+    },
+    "faculty": {
+        "lab": "أعضاء هيئة تدريس {}",
+        "Find_wd": True,
+        "example": "",
+    },
+    "trustees": {
+        "lab": "أمناء {}",
+        "Find_wd": True,
+        "example": "",
+    },
+}
 
 to_get_startswith = {
-    "21st century members of ": {"lab": "أعضاء {} في القرن 21", "Find_wd": False},
-    "20th century members of ": {"lab": "أعضاء {} في القرن 20", "Find_wd": False},
-    "19th century members of ": {"lab": "أعضاء {} في القرن 19", "Find_wd": False},
-    "18th century members of ": {"lab": "أعضاء {} في القرن 18", "Find_wd": False},
-    "17th century members of ": {"lab": "أعضاء {} في القرن 17", "Find_wd": False},
-    "21st century women members of ": {"lab": "عضوات {} في القرن 21", "Find_wd": False},
-    "20th century women members of ": {"lab": "عضوات {} في القرن 20", "Find_wd": False},
-    "19th century women members of ": {"lab": "عضوات {} في القرن 19", "Find_wd": False},
-    "18th century women members of ": {"lab": "عضوات {} في القرن 18", "Find_wd": False},
-    "17th century women members of ": {"lab": "عضوات {} في القرن 17", "Find_wd": False},
+    "association football matches navigational boxes by teams:": {
+        "lab": "صناديق تصفح مباريات كرة قدم حسب الفرق:{}",
+        "Find_wd": False,
+        "example": "Category:Association football matches navigational boxes by teams:Egypt",
+    },
+    "21st century members of ": {
+        "lab": "أعضاء {} في القرن 21",
+        "Find_wd": False,
+        "example": "Category:21st-century members of the Louisiana State Legislature",
+    },
+    "20th century members of ": {"lab": "أعضاء {} في القرن 20", "Find_wd": False, "example": ""},
+    "19th century members of ": {"lab": "أعضاء {} في القرن 19", "Find_wd": False, "example": ""},
+    "18th century members of ": {"lab": "أعضاء {} في القرن 18", "Find_wd": False, "example": ""},
+    "17th century members of ": {"lab": "أعضاء {} في القرن 17", "Find_wd": False, "example": ""},
+    "21st century women members of ": {"lab": "عضوات {} في القرن 21", "Find_wd": False, "example": ""},
+    "20th century women members of ": {"lab": "عضوات {} في القرن 20", "Find_wd": False, "example": ""},
+    "19th century women members of ": {"lab": "عضوات {} في القرن 19", "Find_wd": False, "example": ""},
+    "18th century women members of ": {"lab": "عضوات {} في القرن 18", "Find_wd": False, "example": ""},
+    "17th century women members of ": {"lab": "عضوات {} في القرن 17", "Find_wd": False, "example": ""},
+    "presidents of ": {"lab": "رؤساء {}", "Find_wd": False, "example": ""},
+    "family of ": {"lab": "عائلة {}", "Find_wd": False, "example": ""},
+    "lists of ": {"lab": "قوائم {}", "Find_wd": False, "example": ""},
+    "children of ": {"lab": "أطفال {}", "Find_wd": False, "example": ""},
+    "discoveries by ": {"lab": "اكتشافات بواسطة {}", "Find_wd": True, "example": ""},
 }
 
 
 def get_episodes(category3, category3_nolower):
-    '''
+    """
     examples:
     Category:2016 American television episodes
     Category:Game of Thrones (season 1) episodes
     Category:Game of Thrones season 1 episodes
-    '''
+    """
 
     list_of_cat = ""
 
@@ -61,7 +119,7 @@ def get_episodes(category3, category3_nolower):
     return list_of_cat, category3
 
 
-def get_from_dict(category3):
+def get_from_starts_dict(category3):
     list_of_cat = ""
     Find_wd = False
 
@@ -78,6 +136,52 @@ def get_from_dict(category3):
     return category3, list_of_cat, Find_wd
 
 
+def get_from_endswith_dict(category3):
+    list_of_cat = ""
+    Find_wd = False
+
+    for key, tab in to_get_endswith.items():
+        lab = tab["lab"]
+
+        if category3.endswith(key):
+            list_of_cat = lab
+            category3 = category3.replace(key, "", 1)
+            if tab.get("Find_wd") is True:
+                Find_wd = True
+            break
+
+    return category3, list_of_cat, Find_wd
+
+
+def get_templates_fo(category3):
+    """
+    examples:
+    Category:2016 American television episodes
+    Category:Game of Thrones (season 1) episodes
+    Category:Game of Thrones season 1 episodes
+    """
+
+    list_of_cat = ""
+
+    dict_temps = {
+        "sidebar templates": "قوالب اشرطة جانبية {}",
+        "politics and government templates": "قوالب سياسة وحكومة {}",
+        "infobox templates": "قوالب معلومات {}",
+    }
+
+    for key, lab in dict_temps.items():
+        if category3.endswith(key):
+            list_of_cat = lab
+            category3 = category3.replace(key, "", 1).strip()
+            break
+
+    if not list_of_cat:
+        list_of_cat = "قوالب {}"
+        category3 = category3.replace(" templates", "", 1).strip()
+
+    return list_of_cat, category3
+
+
 def get_list_of_and_cat3(category3, category3_nolower):
     foot_ballers = False
     Find_wd = False
@@ -85,14 +189,18 @@ def get_list_of_and_cat3(category3, category3_nolower):
     category_lab = ""
     list_of_cat = ""
 
-    print(f"get_list_of_and_cat3: {category3=}\n" * 10)
+    # print(f"get_list_of_and_cat3: {category3=}\n" * 10)
 
-    category3, list_of_cat, Find_wd = get_from_dict(category3)
+    category3, list_of_cat, Find_wd = get_from_starts_dict(category3)
 
     if not list_of_cat:
         if category3.startswith("women members of "):
             list_of_cat = "عضوات {}"
             category3 = category3.replace("women members of ", "", 1)
+
+        elif category3.endswith(" episodes"):
+            Find_wd = True
+            list_of_cat, category3 = get_episodes(category3, category3_nolower)
 
         elif category3.endswith(" footballers"):
             foot_ballers = True
@@ -119,31 +227,6 @@ def get_list_of_and_cat3(category3, category3_nolower):
                 list_of_cat = "لاعبو {}"
                 category3 = category3_nolower.replace(" footballers", "", 1)
 
-        elif category3.startswith("presidents of "):
-            list_of_cat = "رؤساء {}"
-            category3 = category3.replace("presidents of ", "", 1)
-
-        elif category3.startswith("family of "):
-            list_of_cat = "عائلة {}"
-            category3 = category3.replace("family of ", "", 1)
-
-        elif category3.startswith("discoveries by "):
-            Find_wd = True
-            list_of_cat = "اكتشافات بواسطة {}"
-            category3 = category3.replace("discoveries by ", "", 1)
-
-        elif category3.startswith("lists of "):
-            list_of_cat = "قوائم {}"
-            category3 = category3.replace("lists of ", "", 1)
-
-        elif category3.startswith("children of "):
-            list_of_cat = "أطفال {}"
-            category3 = category3.replace("children of ", "", 1)
-
-        elif category3.startswith("association football matches navigational boxes by teams:"):
-            list_of_cat = "صناديق تصفح مباريات كرة قدم حسب الفرق:{}"
-            category3 = category3.replace("association football matches navigational boxes by teams:", "", 1)
-
         elif category3.endswith(" squad templates"):
             list_of_cat = "قوالب تشكيلات {}"
             category3 = category3.replace(" squad templates", "", 1)
@@ -151,6 +234,9 @@ def get_list_of_and_cat3(category3, category3_nolower):
             if cate_labs:
                 category_lab = cate_labs
                 list_of_cat = "قوالب {}"
+
+        elif category3.endswith(" templates"):
+            list_of_cat, category3 = get_templates_fo(category3)
 
         elif category3.endswith(" squad navigational boxes"):
             list_of_cat = "قوالب تصفح تشكيلات {}"
@@ -163,24 +249,6 @@ def get_list_of_and_cat3(category3, category3_nolower):
         elif category3.endswith(" stubs") and Find_stubs[1]:
             list_of_cat = "بذرة {}"
             category3 = category3.replace(" stubs", "", 1)
-        elif category3.endswith(" alumni"):
-            Find_wd = True
-            list_of_cat = "خريجو {}"
-            category3 = category3.replace(" alumni", "", 1)
-
-        elif category3.endswith(" board members"):
-            Find_wd = True
-            list_of_cat = "أعضاء مجلس {}"
-            category3 = category3.replace(" board members", "", 1)
-        elif category3.endswith(" faculty"):
-            Find_wd = True
-            list_of_cat = "أعضاء هيئة تدريس {}"
-            category3 = category3.replace(" faculty", "", 1)
-
-        elif category3.endswith(" trustees"):
-            Find_wd = True
-            list_of_cat = "أمناء {}"
-            category3 = category3.replace(" trustees", "", 1)
 
         elif category3.endswith(" award winners"):
             list_of_cat = "حائزو جوائز {}"
@@ -198,60 +266,29 @@ def get_list_of_and_cat3(category3, category3_nolower):
             list_of_cat = "مخططات {}"
             category3 = category3.replace(" charts", "", 1)
 
-        elif category3.endswith(" sidebar templates"):
-            list_of_cat = "قوالب أشرطة جانبية {}"
-            category3 = category3.replace(" sidebar templates", "", 1)
-
-        elif category3.endswith(" politics and government templates"):
-            list_of_cat = "قوالب سياسة وحكومة {}"
-            category3 = category3.replace(" politics and government templates", "", 1)
-        elif category3.endswith("c. playerss") or category3.endswith("c. playerss"):
+        elif category3.endswith(" players") or category3.endswith(" playerss"):
             Find_wd = True
             Find_ko = True
             list_of_cat = "لاعبو {}"
-            category3 = category3_nolower.replace(" playerss", "", 1)
-        elif category3.endswith("c. players") or category3.endswith("c. players"):
-            Find_ko = True
-            Find_wd = True
-            list_of_cat = "لاعبو {}"
-            category3 = category3_nolower.replace(" players", "", 1)
 
-        elif category3.endswith(" episodes"):
-            Find_wd = True
-            list_of_cat, category3 = get_episodes(category3, category3_nolower)
-
-        elif category3.endswith(" playerss"):
-            Find_ko = True
-            Find_wd = True
-            if category3.endswith(" playerss"):
-                list_of_cat = "لاعبو {}"
+            if category3.endswith("c. playerss"):
                 category3 = category3_nolower.replace(" playerss", "", 1)
 
-        elif category3.endswith(" players"):
-            Find_ko = True
-            Find_wd = True
-            if category3.endswith(" players"):
+            elif category3.endswith("c. players"):
                 list_of_cat = "لاعبو {}"
                 category3 = category3_nolower.replace(" players", "", 1)
 
-        elif category3.endswith(" sports navigational boxes"):
-            list_of_cat = "صناديق تصفح الرياضة في {}"
-            category3 = category3.replace(" sports navigational boxes", "", 1)
+            elif category3.endswith(" playerss"):
+                list_of_cat = "لاعبو {}"
+                category3 = category3_nolower.replace(" playerss", "", 1)
+
+            elif category3.endswith(" players"):
+                list_of_cat = "لاعبو {}"
+                category3 = category3_nolower.replace(" players", "", 1)
 
         elif category3.endswith(" events"):
             list_of_cat = "أحداث {}"
             category3 = category3.replace(" events", "", 1)
-        elif category3.endswith(" navigational boxes"):
-            list_of_cat = "صناديق تصفح {}"
-            category3 = category3.replace(" navigational boxes", "", 1)
-
-        elif category3.endswith(" infobox templates"):
-            list_of_cat = "قوالب معلومات {}"
-            category3 = category3.replace(" infobox templates", "", 1)
-
-        elif category3.endswith(" templates"):
-            list_of_cat = "قوالب {}"
-            category3 = category3.replace(" templates", "", 1)
 
         elif category3.endswith(" tournaments"):
             list_of_cat = "بطولات {}"
@@ -268,6 +305,9 @@ def get_list_of_and_cat3(category3, category3_nolower):
             # "تصنيف:معلقو الدوري الرئيسي لكرة القدم داخل الصالات (1978–1992)",
             list_of_cat = "معلقو {}"
             category3 = category3.replace(" commentators", "", 1)
+
+    if not list_of_cat:
+        category3, list_of_cat, Find_wd = get_from_endswith_dict(category3)
 
     if list_of_cat:
         print_put(f'<<lightblue>> list_of_cat:"{list_of_cat}", category3:"{category3}",Find_wd:{str(Find_wd)},Find_ko:{str(Find_ko)} ')
