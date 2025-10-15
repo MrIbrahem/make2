606b07e474df5a2cab604df8db40b29e39e9005c
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-16 01:23:56 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .src import event, printe, do_print_options, print_memory
+
+__all__ = [
+    "printe",
+    "event",
+    "do_print_options",
+    "print_memory",
+]
