1b4bcd245cfb58e2a590698fbef0fdf3141579e2
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-16 02:10:23 +0300

    .

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from .src import event, printe, do_print_options, print_memory
+from .src import event, printe, do_print_options, print_memory, new_func_lab
 
 __all__ = [
     "printe",
+    "new_func_lab",
     "event",
     "do_print_options",
     "print_memory",
--- src/bot.py
+++ src/bot.py
@@ -12,6 +12,7 @@ from typing import Dict, Any, List
 from . import main
 from .media_bots import films_bot  # test_films
 
+new_func_lab = main.new_func_lab
 
 def event(tab: Dict[str, Any], return_no_labs: bool = False, **kwargs: Any) -> List[str]:
     return main.event(tab, return_no_labs=return_no_labs, **kwargs)
