5e6f563a09ee30fb03a42c77f3490333f6cd7625
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-11-09 03:08:53 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from .src import event, printe, do_print_options, print_memory, new_func_lab, dump_all_len
+from .src import event, printe, do_print_options, print_memory, new_func_lab
+from ma_lists import dump_all_len
 
 __all__ = [
     "printe",
